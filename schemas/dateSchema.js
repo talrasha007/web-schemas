96f742fc3dfc6cec958db97cd225dfc2a27eaa80
talrasha007 <talrasha007@gmail.com>
2014-05-11 17:12:44 +0800

    add generator support for generators.

--- schemas/dateSchema.js
+++ schemas/dateSchema.js
@@ -10,8 +10,13 @@ util.inherits(DateSchema, cm.ComparableSchema);
 _.extend(DateSchema.prototype, {
     parse: function (str) {
         if (str === undefined) return ;
+
         var r = Date.parse(str);
         if (!isNaN(r)) return new Date(r);
+
+        r = new Date(str * 1);
+        if (!isNaN(r.getTime())) return r;
+
         throw new cm.SchemaParseError('Should be a date.');
     }
 });
